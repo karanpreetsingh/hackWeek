4c99a0594a1f672e3a601502a50d3188532b20d0
Geetanjali Goyal <geetanjali.goyal@proptiger.com>
2018-02-21 16:23:36 +0530

    Add accordion

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 $('.filter').width($('.result-content').width()-1);
 
+$('.accordion').width($(window).width() - $('.result-content').width()-60);
+
+$('#map').width($('.accordion').width());
 $(window).resize(function(){
     $('.filter').width($('.result-content').width()-1);
 });
@@ -138,3 +141,39 @@ function myMap() {
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 } 
+
+var acc = document.getElementsByClassName("accordion");
+
+
+var i;
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+} 
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}

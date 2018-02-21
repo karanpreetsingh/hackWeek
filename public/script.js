6d31b58fca812bc4782e19fed94da96022a3b8f4
Karanpreet <karan.preet@hotmail.com>
2018-02-21 13:01:43 +0530

    Add map and iterny btn

--- public/script.js
+++ public/script.js
@@ -129,3 +129,12 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 } 
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: 'roadmap'
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+} 
